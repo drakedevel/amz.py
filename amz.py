739554de6d9d6aca6b9ae047577094170fce445b
Andrew Drake <adrake@adrake.org>
2012-06-25 17:16:18 -0400

    Update to Python 3.

--- amz.py
+++ amz.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import xml.etree.ElementTree as ET
 import sys
 
@@ -8,14 +8,14 @@ def gettracks(elem):
         artist = t.find('creator').text
         album = t.find('album').text
         loc = t.find('location').text.replace('+', ' ')
-        print "mkdir -p '%s/%s'" % (artist, album)
-        print "wget -P '%s/%s' '%s'" % (artist, album, loc)
+        print("mkdir -p '{0}/{1}'".format(artist, album))
+        print("wget -P '{0}/{1}' '{2}'".format(artist, album, loc))
 
 def process(filename):
     tree = ET.parse(filename)
     for ext in tree.iterfind('extension'):
         deluxe = ext.find('deluxe')
-        if deluxe:
+        if deluxe is not None:
             gettracks(deluxe)
     gettracks(tree)
 
@@ -23,4 +23,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         process(sys.argv[1])
     else:
-        print "Usage: amz.py foo.amz"
+        print("Usage: amz.py foo.amz")
