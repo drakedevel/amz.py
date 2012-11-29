03d3796168bbbcebcbb521899f6c84401697dd9f
Andrew Drake <adrake@adrake.org>
2012-11-28 19:52:41 -0800

    Support new AMZ files.

--- amz.py
+++ amz.py
@@ -7,9 +7,16 @@ def gettracks(elem):
     for t in tracklist.iterfind('track'):
         artist = t.find('creator').text
         album = t.find('album').text
-        loc = t.find('location').text.replace('+', ' ')
+        track = int(t.find('trackNum').text)
+        title = t.find('title').text
+        fileType = 'mp3'
+        for m in t.iterfind('meta'):
+            if m.attrib['rel'] == 'http://www.amazon.com/dmusic/trackType':
+                fileType = m.text
+                break
+        loc = t.find('location').text
         print("mkdir -p '{0}/{1}'".format(artist, album))
-        print("wget -P '{0}/{1}' '{2}'".format(artist, album, loc))
+        print("wget -U'Amazon MP3 Downloader (Win32 1.0.17 en_US)' -O '{0}/{1}/{2:02d} - {3}.{4}' '{5}'".format(artist, album, track, title, fileType, loc))
 
 def process(filename):
     tree = ET.parse(filename)
